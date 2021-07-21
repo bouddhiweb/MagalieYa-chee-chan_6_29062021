7c500a036b579e369206931f0e077df53fc3b8a4
bouddhiweb <bouddhi.web@gmail.com>
2021-07-21 17:59:49 +0200

    modification user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,70 +1,84 @@
+// On retrouve ici la logique métier en lien avec nos utilisateurs, appliqué aux routes POST pour les opérations d'inscription et de connexion
+
+// On utilise l'algorithme bcrypt pour hasher le mot de passe des utilisateurs
 const bcrypt = require('bcrypt')
+
+// On récupère notre model User ,créer avec le schéma mongoose
+const User = require('../models/user');
+
+// On utilise le package jsonwebtoken pour attribuer un token à un utilisateur au moment ou il se connecte
 const jwt = require('jsonwebtoken');
-const User = require('../models/User')
 
-const TOKEN = process.env.TOKEN;
+// Middleware pour crée un nouvel utilisateur
 
-/**  Fonction pour le signup et le login **/
-
-// SIGNUP
-
+// On sauvegarde un nouvel utilisateur et crypte son mot de passe avec un hash généré par bcrypt
 exports.signup = (req, res, next) => {
-    // Password refusé
-    if ('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$') {   // Huit caractères au minimum, au moins une lettre et un chiffre:
-        console.error('Le mot de passe doit contenir des lettres et au moins 1 chiffre (8 caractères minimum)');
-        return res.status(401).json({ error: 'Le mot de passe doit contenir des lettres et au moins 1 chiffre (8 caractères minimum)' });
+    // Password is not acceptable
+    if (!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.{6,})/.test(req.body.password)) {   // Test password strength
+        return res.status(401).json({ error: 'Le mot de passe doit contenir une lettre majuscule, une minuscule et au moins 1 chiffre (6 caractères min)' });
     } else {
-        // Hash du MDP
+        // Password is acceptable, hash it
         bcrypt.hash(req.body.password, 10)
             .then(hash => {
                 const user = new User({
                     email: req.body.email,
                     password: hash
                 })
-                // Envoie du user
                 user.save()
-                    .then(() => res.status(201).json({ message: 'Utilisateur créé : ' + user.email }))
+                    .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
                     .catch(error => res.status(400).json({ error }));
             })
-            .catch(error => res.status(500).json(
-                { error }
-            ));
+            .catch(error => res.status(500).json({ error }));
     }
 };
 
-// LOGIN
-
+// Le Middleware pour la connexion d'un utilisateur vérifie si l'utilisateur existe dans la base MongoDB lors du login
+//si oui il vérifie son mot de passe, s'il est bon il renvoie un TOKEN contenant l'id de l'utilisateur, sinon il renvoie une erreur
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email }) // Find user.
+    // On doit trouver l'utilisateur dans la BDD qui correspond à l'adresse entrée par l'utilisateur
+    User.findOne({
+        email: req.body.email
+    })
         .then(user => {
-            if (!user) { // If user not found
-                console.error('Utilisateur non trouvé !');
-                return res.status(401).json({ error: 'Utilisateur non trouvé ou mot de passe incorrect.' });
+            // Si on trouve pas l'utilisateur on va renvoyer un code 401 "non autorisé"
+            if (!user) {
+                return res.status(401).json({
+                    error: 'Utilisateur non trouvé !'
+                });
             }
-            bcrypt.compare(req.body.password, user.password) // If user found, compare password send in res and user
-                .then(valid => { // Boolean
-                    if (!valid) { //False
-                        console.error('Mot de passe incorrect !');
-                        return res.status(401).json({ error: 'Utilisateur non trouvé ou mot de passe incorrect.' });
+            // On utilise bcrypt pour comparer les hashs et savoir si ils ont la même string d'origine
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    // Si false, c'est que ce n'est pas le bon utilisateur, ou le mot de passe est incorrect
+                    if (!valid) {
+                        return res.status(401).json({
+                            error: 'Mot de passe incorrect !'
+                        });
                     }
-                    res.status(200).json({ //True
+                    // Si true, on renvoie un statut 200 et un objet JSON avec un userID + un token
+                    res.status(200).json({ // Le serveur backend renvoie un token au frontend
                         userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            TOKEN,
-                            { expiresIn: '8h' }
+                        // Permet de vérifier si la requête est authentifiée
+                        // on va pouvoir obtenir un token encodé pour cette authentification grâce à jsonwebtoken, on va créer des tokens et les vérifier
+                        token: jwt.sign( // Encode un nouveau token avec une chaine de développement temporaire
+                            {
+                                userId: user._id
+                            }, // Encodage de l'userdID nécéssaire dans le cas où une requête transmettrait un userId (ex: modification d'une sauce)
+                            'RANDOM_TOKEN_SECRET', // Clé d'encodage du token qui peut être rendue plus complexe en production
+                            // Argument de configuration avec une expiration au bout de 24h
+                            {
+                                expiresIn: '24h'
+                            }
                         )
+                        // On encode le userID pour la création de nouveaux objets, et cela permet d'appliquer le bon userID
+                        // aux objets et ne pas modifier les objets des autres
                     });
                 })
-                .catch(error => {
-                    console.log(error);
-                    res.status(500).json(
-                    { error }
-                    )
-                });
+                .catch(error => res.status(500).json({
+                    error
+                }));
         })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json( { error })
-        });
+        .catch(error => res.status(500).json({
+            error
+        }));
 };
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,36 @@
+// Création d'un model user avec mongoose, on importe donc mongoose
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const validator = require('validator');
 
-/** Mongoose Schema for the user **/
 
+// On rajoute ce validateur comme plugin
+const uniqueValidator = require('mongoose-unique-validator'); // package qui valide l'unicité de l'email
+
+
+// la valeur unique , avec l'élément mongoose-unique-validator passé comme plug-in,
+// s'assurera que deux utilisateurs ne peuvent partager la même adresse e-mail.
+// Utilisation d'une expression régulière pour valider le format d'email.
+// Le mot de passe fera l'objet d'une validation particulière grâce au middleware verifPasword et au model password
+
+// On crée notre schéma de données dédié à l'utilisateur
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true, validate: [validator.isEmail, { error: 'adresse mail non valide' }] },
-    password: { type: String, required: true },
+    // L'email doit être unique
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Veuillez entrer votre adresse email"],
+        match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Veuillez entrer une adresse email correcte"]
+    },
+    // enregistrement du mot de pass
+    password: {
+        type: String,
+        required: [true, "Veuillez choisir un mot de passe"]
+    }
 });
 
-userSchema.plugin(uniqueValidator);
-
+// On exporte ce schéma sous forme de modèle : le modèle s'appellera user et on lui passe le schéma de données
 module.exports = mongoose.model('User', userSchema);
+
+// Pour s'assurer que deux utilisateurs ne peuvent pas utiliser la même adresse e-mail
+// nous utiliserons le mot clé unique pour l'attribut email du schéma d'utilisateur userSchema.
+// Les erreurs générées par défaut par MongoDB pouvant être difficiles à résoudre, nous installerons un package de validation
+//pour pré-valider les informations avant de les enregistrer : npm install --save mongoose-unique-validator
