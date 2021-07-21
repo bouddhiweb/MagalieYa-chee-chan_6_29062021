8c113f55015fa1a8d3d20801fc98a63adb777502
bouddhiweb <bouddhi.web@gmail.com>
2021-07-21 13:55:05 +0200

    modification user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,22 +10,25 @@ const TOKEN = process.env.TOKEN;
 
 exports.signup = (req, res, next) => {
     // Password refusé
-    if ("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$") {   // Huit caractères au minimum, au moins une lettre et un chiffre:
-        window.alert('Le mot de passe doit contenir des lettres et au moins 1 chiffre (8 caractères minimum)');
+    if ('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$') {   // Huit caractères au minimum, au moins une lettre et un chiffre:
+        console.error('Le mot de passe doit contenir des lettres et au moins 1 chiffre (8 caractères minimum)');
         return res.status(401).json({ error: 'Le mot de passe doit contenir des lettres et au moins 1 chiffre (8 caractères minimum)' });
     } else {
-        // Password is acceptable, hash it
+        // Hash du MDP
         bcrypt.hash(req.body.password, 10)
             .then(hash => {
                 const user = new User({
                     email: req.body.email,
                     password: hash
                 })
+                // Envoie du user
                 user.save()
                     .then(() => res.status(201).json({ message: 'Utilisateur créé : ' + user.email }))
                     .catch(error => res.status(400).json({ error }));
             })
-            .catch(error => res.status(500).json({ error }));
+            .catch(error => res.status(500).json(
+                { error }
+            ));
     }
 };
 
@@ -35,13 +38,13 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email }) // Find user.
         .then(user => {
             if (!user) { // If user not found
-                window.alert('Utilisateur non trouvé !');
+                console.error('Utilisateur non trouvé !');
                 return res.status(401).json({ error: 'Utilisateur non trouvé ou mot de passe incorrect.' });
             }
             bcrypt.compare(req.body.password, user.password) // If user found, compare password send in res and user
                 .then(valid => { // Boolean
                     if (!valid) { //False
-                        window.alert('Mot de passe incorrect !');
+                        console.error('Mot de passe incorrect !');
                         return res.status(401).json({ error: 'Utilisateur non trouvé ou mot de passe incorrect.' });
                     }
                     res.status(200).json({ //True
@@ -55,11 +58,13 @@ exports.login = (req, res, next) => {
                 })
                 .catch(error => {
                     console.log(error);
-                    res.status(500).json({ error })
+                    res.status(500).json(
+                    { error }
+                    )
                 });
         })
         .catch(error => {
             console.log(error);
-            res.status(500).json({ error })
+            res.status(500).json( { error })
         });
 };
