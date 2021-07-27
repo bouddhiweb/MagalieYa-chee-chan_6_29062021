78699487041e6d09f02ad300b4a9277c35583308
bouddhiweb <bouddhi.web@gmail.com>
2021-07-27 11:19:01 +0200

    opmtimisation sauces.js

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -108,6 +108,6 @@ exports.likeSauce = (req, res, next) => {
                 .catch((error) => { res.status(400).json({ error: error }); });
             break;
         default:
-            console.error('mauvaise requête');
+            res.status(400).json({ error: 'Paramètre like attendu : -1, 0 ou 1' })
     }
 }
